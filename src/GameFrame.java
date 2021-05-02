c1df18661cc3d3083cd536053ccf19025d67dc41
Peter Meszaros <meszarosp.peter@gmail.com>
2021-05-02 18:10:17 +0200

    LevelView

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -10,9 +10,13 @@ public class GameFrame extends JFrame {
 
     public GameFrame() {
         super();
+        setTitle("Asteroid Game");
+        setMinimumSize(new Dimension(1000, 600));
+        setPreferredSize(new Dimension(1000, 600));
         lv = new LevelView();
         iv = new InventoryView();
         add(lv, BorderLayout.CENTER);
         add(iv, BorderLayout.SOUTH);
+        pack();
     }
 }
